d4ff6bb825b46ec08cf1fb2c50739016f146d317
Viola8905 <viola89055@gmail.com>
2022-04-19 17:39:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Typography,
+  Select,
+  MenuItem,
+  ListSubheader,
+  InputLabel,
+  FormControl,
+} from "@mui/material";
+import Data from "./Data/Data";
+import Map from "./Components/Map";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState("");
+  const handleChange = (name) => {
+    console.log("value changed");
+  };
+
+  // console.log(data)
+
+  function Filter() {
+    let ch;
+    Data.map((obj) =>
+      obj.churches.map((church) => {
+        if (church.name == data) {
+          ch = church;
+        }
+      })
+    );
+
+    console.log(ch.img);
+    return (
+      <div>
+        <div>{ch.name}</div>
+        <div>{ch.address}</div>
+        {ch.src < 20 ? (
+          <div> No map </div>
+        ) : (
+          <iframe
+            src={ch.src}
+            style={{ border: "0", width: "500px", height: "500px", marginTop:"20px" }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        
+        )}
+        {/* <div>
+
+				<img src={"./img/свято-троїцький-ужгород.jpg"} />
+				</div> */}
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Map />
+      {/* <FormControl variant="filled" sx={{ m: 1, minWidth: "300px" }}>
+        <InputLabel id="locality-label">Населений пункт</InputLabel>
+        <Select
+          labelId="locality-label"
+          value={data}
+          onClick={() => handleChange()}
         >
-          Learn React
-        </a>
-      </header>
+          {Data.map((city) => (
+            <div key={city.city}>
+              <ListSubheader>{city.city}</ListSubheader>
+              {city.churches.map((church) => (
+                <MenuItem
+                  key={church.name}
+                  value={church.name}
+                >
+                  {church.name}
+                </MenuItem>
+              ))}
+            </div>
+          ))}
+        </Select>
+      </FormControl> */}
+      <div style={{ margin: "50px auto" }}>
+        <InputLabel id="demo-simple-select-label">UPZ Churches</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={data}
+          label="Data"
+          onChange={(e) => setData(e.target.value)}
+          style={{ margin: "10px auto", width: "50%" }}
+        >
+          {Data.map((city) =>
+            city.churches.map((church) => (
+              <MenuItem key={church.name} value={church.name}>
+                <ListSubheader>{city.city}</ListSubheader>
+                {church.name}
+              </MenuItem>
+            ))
+          )}
+        </Select>
+      </div>
+      {data ? Filter() : <></>}
+      
     </div>
   );
 }
---
+++ src/Data/Data.js
@@ -0,0 +1,262 @@
+import Img from "../img/свято-троїцький-ужгород.jpg";
+const Data = [
+  {
+    city: "Ужгород",
+    churches: [
+      {
+        name: "Єпархіальний Храм прп. Амфілохія Почаївського",
+        address: "вул.Станційна, 16а (неподалік залізничного вокзалу).",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d392.13873209936406!2d22.299802104735292!3d48.6101412523904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473919081e53311d%3A0x73ccaa06b3862fff!2sZakarpat%C2%B7s%CA%B9ka%20Yeparkhiya%20Ptsu!5e0!3m2!1sru!2spl!4v1650312198447!5m2!1sru!2spl",
+        img: "https://www.google.pl/maps/place/Zakarpat%C2%B7s%CA%B9ka+Yeparkhiya+Ptsu/@48.6101207,22.2998896,3a,75y/data=!3m8!1e2!3m6!1sAF1QipOWbYDBHHafK9q1vTzE3Pw9WPA8nEQ3M7ZByIWa!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipOWbYDBHHafK9q1vTzE3Pw9WPA8nEQ3M7ZByIWa%3Dw224-h298-k-no!7i1200!8i1600!4m15!1m7!3m6!1s0x473919a702c4553b:0x1c2fd3d0a59b0184!2z0YPQuy4g0KHRgtCw0L3RhtC40L7QvdC90LDRjywgMTbQkCwg0KPQttCz0L7RgNC-0LQsINCX0LDQutCw0YDQv9Cw0YLRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwg0KPQutGA0LDQuNC90LAsIDg4MDAw!3b1!8m2!3d48.6102258!4d22.2998915!3m6!1s0x473919081e53311d:0x73ccaa06b3862fff!8m2!3d48.6101207!4d22.2998896!14m1!1BCgIgAQ#",
+      },
+      {
+        name: "Храм Святої Тройці (м-н Шахта)",
+        address: "вул.Тімірязєва, 8 (за обласним кардіодиспансером)",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2217.1181511868376!2d22.307436419394534!3d48.63639336905996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473919cd851065cf%3A0x4068036ca0d5483f!2sSvyato%20Troyits%CA%B9kyy%20Khram!5e0!3m2!1sru!2spl!4v1650313238193!5m2!1sru!2spl",
+        img: "https://www.google.com/maps/place/Svyato+Troyits%CA%B9kyy+Khram/@48.6374573,22.3060651,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipNVenCEHR9NQS4BMuoMPWS2LKXHOSRv0YXiHOTq!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNVenCEHR9NQS4BMuoMPWS2LKXHOSRv0YXiHOTq%3Dw114-h86-k-no!7i4160!8i3120!4m13!1m7!3m6!1s0x473919cc200c2abf:0xbb1b2375f2dcff52!2z0YPQuy4g0KLQuNC80LjRgNGP0LfQtdCy0LAsIDgsINCj0LbQs9C-0YDQvtC0LCDQl9Cw0LrQsNGA0L_QsNGC0YHQutCw0Y8g0L7QsdC70LDRgdGC0YwsINCj0LrRgNCw0LjQvdCwLCA4ODAwMA!3b1!8m2!3d48.6340134!4d22.3063432!3m4!1s0x473919cd851065cf:0x4068036ca0d5483f!8m2!3d48.6374482!4d22.3061027#",
+      },
+      {
+        name: "Свято-Троїцький храм.",
+        address:
+          "вул.Київська набережна, 16 (обабіч театру ім.братів Шерегіїв)",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2637.438665357658!2d22.263901144185706!3d48.62058722653705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473919a3b38202a7%3A0x7b8a657fdbb9568!2z0KHQstGP0YLQviDQotGA0L7QuNGG0LrQuNC5INGF0YDQsNC8!5e0!3m2!1sru!2sua!4v1650375121159!5m2!1sru!2sua",
+        img:"./img/свято-троїцький-ужгород.jpg",
+      },
+      {
+        name: "Храм Преображення Господнього",
+        address: " вул.Р.Шухевича,8 Б.",
+        src: "",
+        img: "",
+      },
+      {
+        name: " Храм св.першомученика і архідиякона Стефана.",
+        address: " с.Минай,Ужгородського району вул.Коротка,1 Д.",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3742.8558317167704!2d22.262895563531977!3d48.62051934379529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473919a3b38202a7%3A0x7b8a657fdbb9568!2z0KHQstGP0YLQviDQotGA0L7QuNGG0LrQuNC5INGF0YDQsNC8!5e0!3m2!1sru!2sua!4v1650378947671!5m2!1sru!2sua",
+        img: "",
+      },
+    ],
+  },
+  {
+    city: "Перечин",
+    churches: [
+      {
+        name: " Храм Покрови Пресвятої Богородиці",
+        address: "вул.Лермонтова,14",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1556.3650309182394!2d22.489910616970356!3d48.74128547780134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4739130b82af8a3f%3A0x8275fbe767cdbf74!2sUkrayins%CA%B9ka%20Pravoslavna%20Tserkva%20Kyyivs%CA%B9koho%20Patriarkhatu!5e0!3m2!1sru!2sua!4v1650275719453!5m2!1sru!2sua",
+        img: "https://www.google.com/maps/place/Ukrayins%CA%B9ka+Pravoslavna+Tserkva+Kyyivs%CA%B9koho+Patriarkhatu/@48.7422306,22.489856,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipOP2BZ7CRXULLtF84BexujNYYYJpwPFRg3VTML6!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipOP2BZ7CRXULLtF84BexujNYYYJpwPFRg3VTML6%3Dw86-h114-k-no!7i1200!8i1600!4m8!1m2!2m1!1z0LLRg9C7LtCb0LXRgNC80L7QvdGC0L7QstCwLDE0LCDQn9C10YDQtdGH0LjQvSwg0JfQsNC60LDRgNC_0LDRgtGB0LrQsNGPINC-0LHQu9Cw0YHRgtGMINCl0YDQsNC8INCf0L7QutGA0L7QstC4INCf0YDQtdGB0LLRj9GC0L7RlyDQkdC-0LPQvtGA0L7QtNC40YbRliAg!3m4!1s0x4739130b82af8a3f:0x8275fbe767cdbf74!8m2!3d48.7422253!4d22.4898491#",
+      },
+    ],
+  },
+  {
+    city: "Сімер",
+    churches: [
+      {
+        name: " Храм св.вмч.Параскеви-П'ятниці",
+        address: "вул.Зарічна,46.",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10526.396311181725!2d22.508193269775393!3d48.73225199999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473912ff59b38dbf%3A0x95ddb8541676d43b!2sRelihiyna%20Hromada%20Ukrayins%CA%B9koyi%20Pravoslavnoyi%20Tserkvy%20Svyato-Pokrovs%CA%B9koyi%20Tserkvy%20S.%20Simer%20Perechyns%CA%B9koho%20Rayonu!5e0!3m2!1sru!2sua!4v1650276937921!5m2!1sru!2sua",
+        img: "https://www.google.com/maps/place/Relihiyna+Hromada+Ukrayins%CA%B9koyi+Pravoslavnoyi+Tserkvy+Svyato-Pokrovs%CA%B9koyi+Tserkvy+S.+Simer+Perechyns%CA%B9koho+Rayonu/@48.7322383,22.5167507,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipOvCP_oehvwMZLg_pxOGs15Edo30x_Sp0_Twu4-!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipOvCP_oehvwMZLg_pxOGs15Edo30x_Sp0_Twu4-%3Dw613-h298-k-no!7i4608!8i2240!4m11!1m2!2m1!1z0YHQuNC80LXRgCDRhtC10YDQutCy0LA!3m7!1s0x473912ff59b38dbf:0x95ddb8541676d43b!8m2!3d48.7322535!4d22.5169043!14m1!1BCgIgAQ!15sChfRgdC40LzQtdGAINGG0LXRgNC60LLQsJIBBmNodXJjaA#",
+      },
+    ],
+  },
+  {
+    city: "Мукачево",
+    churches: [
+      {
+        name: " Храм Успіння Пресвятої Богородиці",
+        address: "вул.Я.Мудрого,42 а.",
+        src: "<https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10588.373633712763!2d22.707202369775395!3d48.43554699999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4739ab7fd4163e8d%3A0x62cd39a2c8ca9e66!2sUspens%CA%B9ka%20Tserkva%20Ptsu!5e0!3m2!1sru!2sua!4v1650276783086!5m2!1sru!2sua",
+        img: "https://www.google.com/maps/place/Uspens%CA%B9ka+Tserkva+Ptsu/@48.4355612,22.7159696,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipP6plzXOXjF0vyin5bsyx9dQzZD_QinXUB_Tb5X!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipP6plzXOXjF0vyin5bsyx9dQzZD_QinXUB_Tb5X%3Dw86-h191-k-no!7i1800!8i4000!4m13!1m7!3m6!1s0x4739ac8029c5add9:0x834daef883089b0a!2z0YPQuy4g0K_RgNC-0YHQu9Cw0LLQsCDQnNGD0LTRgNC-0LPQviwgNDIsINCc0YPQutCw0YfQtdCy0L4sINCX0LDQutCw0YDQv9Cw0YLRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwgODk2MTE!3b1!8m2!3d48.4356086!4d22.7164784!3m4!1s0x4739ab7fd4163e8d:0x62cd39a2c8ca9e66!8m2!3d48.4355612!4d22.7159696#",
+      },
+      {
+        name: " Храм Преображення Господнього",
+        address: "вул.І.Франка, бічна",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2222.759560625994!2d22.731656203069534!3d48.41994935044984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4739ac933ec9e26d%3A0xd5d8c7b5e86339fb!2sKhram%20Preobrazhennya%20Hospodn%CA%B9oho%20Ptsu!5e0!3m2!1sru!2sua!4v1650276695079!5m2!1sru!2sua",
+        img: "https://www.google.com/maps/place/Khram+Preobrazhennya+Hospodn%CA%B9oho+Ptsu/@48.4195278,22.7345626,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipNScrFiSukbhgLXd3WtY5eiqNR6xaEQrejadKwd!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNScrFiSukbhgLXd3WtY5eiqNR6xaEQrejadKwd%3Dw203-h333-k-no!7i1079!8i1771!4m15!1m7!3m6!1s0x4739ac95282f7a53:0x898f956bf930f543!2z0LLRg9C70LjRhtGPINCG0LLQsNC90LAg0KTRgNCw0L3QutCwLdCx0ZbRh9C90LAsINCc0YPQutCw0YfQtdCy0L4sINCX0LDQutCw0YDQv9Cw0YLRgdGM0LrQsCDQvtCx0LvQsNGB0YLRjA!3b1!8m2!3d48.4176079!4d22.7320696!3m6!1s0x4739ac933ec9e26d:0xd5d8c7b5e86339fb!8m2!3d48.4198581!4d22.7341196!14m1!1BCgIgAQ#",
+      },
+    ],
+  },
+  {
+    city: "Свалява",
+    churches: [
+      {
+        name: "Храм св. пророка і предтечі Господнього Івана Хрестителя",
+        address: "вул.Асталоша,24.",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1311.454019166516!2d22.992454845795724!3d48.54948496683497!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4739bb02cb330d81%3A0xab6f2c4cb19c2300!2z0LLRg9C70LjRhtGPINCQ0YHRgtCw0LvQvtGI0LAsINCh0LLQsNC70Y_QstCwLCDQl9Cw0LrQsNGA0L_QsNGC0YHRjNC60LAg0L7QsdC70LDRgdGC0YwsIDg5MzAw!5e0!3m2!1sru!2sua!4v1650278513899!5m2!1sru!2sua",
+        img: "Немає фото",
+      },
+    ],
+  },
+  {
+    city: "с.Пилипець",
+    churches: [
+      {
+        name: "Храм Різдва Пресвятої Богородиці",
+        address: " с.Пилипець № 362.",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2580.6762167851216!2d23.33571477887782!3d48.66912347806067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4739eb4175211e61%3A0x6cc6eae1d3108104!2z0JTQtdGA0LXQstGP0L3QvdCw0Y8g0YbQtdGA0LrQvtCy0Ywg0KDQvtC20LTQtdGB0YLQstCwINCf0YDQtdGB0LLRj9GC0L7QuSDQkdC-0LPQvtGA0L7QtNC40YbRiw!5e0!3m2!1sru!2sua!4v1650278974663!5m2!1sru!2sua",
+        img: "https://www.google.com/maps/place/%D0%94%D0%B5%D1%80%D0%B5%D0%B2%D1%8F%D0%BD%D0%BD%D0%B0%D1%8F+%D1%86%D0%B5%D1%80%D0%BA%D0%BE%D0%B2%D1%8C+%D0%A0%D0%BE%D0%B6%D0%B4%D0%B5%D1%81%D1%82%D0%B2%D0%B0+%D0%9F%D1%80%D0%B5%D1%81%D0%B2%D1%8F%D1%82%D0%BE%D0%B9+%D0%91%D0%BE%D0%B3%D0%BE%D1%80%D0%BE%D0%B4%D0%B8%D1%86%D1%8B/@48.6696252,23.338244,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipNfTlIPZWAMlxQRavYPmpx_7BBwwtHsgcRVW5XW!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNfTlIPZWAMlxQRavYPmpx_7BBwwtHsgcRVW5XW%3Dw114-h86-k-no!7i4000!8i3000!4m9!1m2!2m1!1z0YEu0J_QuNC70LjQv9C10YbRjCDRhtC10YDQutCy0LA!3m5!1s0x4739eb4175211e61:0x6cc6eae1d3108104!8m2!3d48.6696252!4d23.338244!15sCiDRgS7Qn9C40LvQuNC_0LXRhtGMINGG0LXRgNC60LLQsJIBBmNodXJjaA!5m1!1e1#",
+      },
+    ],
+  },
+  {
+    city: "Берегово",
+    churches: [
+      {
+        name: "Свято-Троїцько-Михайлівський храм",
+        address: " вул.Можайського,52 В ( біля автостанції)",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1329.7976813181642!2d22.651321102316594!3d48.195147299999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473844d6a3dd715f%3A0x473e34d1eebc92a5!2z0KHQstGP0YLQvi3QotGA0L7QuNGG0LrQvtCc0LjRhdCw0LnQu9C-0LLRgdC60LjQuSDQodC-0LHQvtGA!5e0!3m2!1sru!2sua!4v1650279378059!5m2!1sru!2sua",
+        img: "https://www.google.com/maps/place/%D0%A1%D0%B2%D1%8F%D1%82%D0%BE-%D0%A2%D1%80%D0%BE%D0%B8%D1%86%D0%BA%D0%BE%D0%9C%D0%B8%D1%85%D0%B0%D0%B9%D0%BB%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9+%D0%A1%D0%BE%D0%B1%D0%BE%D1%80/@48.1951473,22.6520536,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipPajNv9sZaOl_Z4VsH0mq0eCOUAK2gqn3pe7kFq!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipPajNv9sZaOl_Z4VsH0mq0eCOUAK2gqn3pe7kFq%3Dw121-h86-k-no!7i1054!8i746!4m9!1m2!2m1!1z0JHQtdGA0LXQs9C-0LLQviDRhtC10YDQutCy0LA!3m5!1s0x473844d6a3dd715f:0x473e34d1eebc92a5!8m2!3d48.1951439!4d22.6520279!15sCh3QkdC10YDQtdCz0L7QstC-INGG0LXRgNC60LLQsJIBD29ydGhvZG94X2NodXJjaA!5m1!1e1#",
+      },
+    ],
+  },
+  {
+    city: "Виноградів",
+    churches: [
+      {
+        name: "Храм Успіння Пресвятої Богородиці ",
+        address: "вул.І.Франка,1.",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2662.4502363165684!2d23.03630831573407!3d48.140124558962455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47383a676f30b953%3A0xa65a818fba1d1474!2sKhram%20Uspinnya%20Presvyatoyi%20Bohorodytsi.%20Upts.!5e0!3m2!1sru!2sua!4v1650279450838!5m2!1sru!2sua",
+        img: "https://www.google.com/maps/place/Khram+Uspinnya+Presvyatoyi+Bohorodytsi.+Upts./@48.140121,23.038497,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipOtBRraS7twKMwQO53dWESMLrI8s-YEmeqJPuf0!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipOtBRraS7twKMwQO53dWESMLrI8s-YEmeqJPuf0%3Dw114-h86-k-no!7i3264!8i2448!4m5!3m4!1s0x47383a676f30b953:0xa65a818fba1d1474!8m2!3d48.140161!4d23.0385061!5m1!1e1#",
+      },
+    ],
+  },
+  {
+    city: "Іршава",
+    churches: [
+      {
+        name: "Храм святих апостолів Петра і Павла ",
+        address: "вул.Шкільна ( на території бувшого районо)",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1106.0822188968766!2d23.047560121082157!3d48.310271655969814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4738353c947f6d5d%3A0x30105f8e1b105b56!2z0LLRg9C7LiDQqNC60ZbQu9GM0L3QsCwg0IbRgNGI0LDQstCwLCDQl9Cw0LrQsNGA0L_QsNGC0YHRjNC60LAg0L7QsdC70LDRgdGC0Yw!5e0!3m2!1sru!2sua!4v1650279696555!5m2!1sru!2sua",
+        img: "Немає фото",
+      },
+    ],
+  },
+  {
+    city: "Хуст",
+    churches: [
+      {
+        name: "Храм св.вмч.Катерини ",
+        address: "вул.Колгоспна,2",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4344.821653717512!2d23.2932380821789!3d48.17418047840452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4738294310c1a739%3A0x900a91ee477ded1f!2sBlahovishchens%CA%B9ka%20Tserkva.%20Upts.!5e0!3m2!1sru!2sua!4v1650280021310!5m2!1sru!2sua", //під питанням
+        img: "немає фото",
+      },
+    ],
+  },
+  {
+    city: "Тячів",
+    churches: [
+      {
+        name: "Храм св.вмч.Варвари",
+        address: " вул.Леонтовича,108.",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1270.981514681455!2d23.570710503947474!3d48.02644271058814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4737912d248897a9%3A0xe1a7fe3e4d4af8a5!2sKhram%20Svyatoyi%20Velykomuchenytsi%20Varvary%2C%20Zakarpat%C2%B7s%CA%B9ka%20Yeparkhiya%20Pravoslavnoyi%20Tserkvy%20Ukrayiny!5e0!3m2!1sru!2sua!4v1650280143348!5m2!1sru!2sua",
+        img: "немає фото",
+      },
+    ],
+  },
+  {
+    city: "смт.Буштино",
+    churches: [
+      {
+        name: "Храм св.вмч.Пантелеймона Цілителя",
+        address: "вул.Головна,444",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17416.10682370713!2d23.47116709391239!3d48.065455931624804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47378f597874b46d%3A0xa0fcccdbefd82c17!2sUkrayins%CA%B9ka%20Pravoslavna%20Tserkva%20Svyatoho%20Velykomuchenyka%20Panteleymona%20Ptsu!5e0!3m2!1sru!2sua!4v1650280257302!5m2!1sru!2sua",
+        img: "https://lh5.googleusercontent.com/p/AF1QipO_fp1N2AgTEcLtCB-vkisAOJ_B7PyzcjdkLs3H=w408-h544-k-no",
+      },
+    ],
+  },
+  {
+    city: "с.Нересниця",
+    churches: [
+      {
+        name: "Храм Різдва св.Івана Предтечі",
+        address: "вул.Головна,444",
+        src: "немає на карті",
+        img: "немає зображення",
+      },
+    ],
+  },
+  {
+    city: "смт.Великий Бичків",
+    churches: [
+      {
+        name: "Храм св.пророка Іллі",
+        address: " вул.Гоголя,2",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17937.89145701369!2d23.987656164709193!3d47.972967155096796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4737a501e528b057%3A0x53cc1811f0139930!2sKhram%20Svyatoho%20Proroka%20Illi%20Ptsu!5e0!3m2!1sru!2sua!4v1650280562776!5m2!1sru!2sua",
+        img: "немає фото",
+      },
+      {
+        name: "Храм Покрови Пресвятої Богородиці",
+        address: "вул.Покровська",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8907.853776371616!2d23.99859633834922!3d47.96797517797461!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4737a5d428e7c309%3A0x8753ff18b1153e7f!2sTserkva%20Ptsu%20Pokrovy%20Presvyatoyi%20Bohorodytsi.!5e0!3m2!1sru!2sua!4v1650280664369!5m2!1sru!2sua",
+        img: "https://www.google.com/maps/place/Tserkva+Ptsu+Pokrovy+Presvyatoyi+Bohorodytsi./@47.9683683,23.9997529,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipMcsctcoCCNPTus-Kxw5wlj1wqn59eNnSSD4kI7!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipMcsctcoCCNPTus-Kxw5wlj1wqn59eNnSSD4kI7%3Dw203-h440-k-no!7i1844!8i4000!4m9!1m2!2m1!1z0YHQvNGCLtCS0LXQu9C40LrQuNC5INCR0LjRh9C60ZbQsiDRhtC10YDQutCy0LA!3m5!1s0x4737a5d428e7c309:0x8753ff18b1153e7f!8m2!3d47.9684909!4d23.9998607!15sCi_RgdC80YIu0JLQtdC70LjQutC40Lkg0JHQuNGH0LrRltCyINGG0LXRgNC60LLQsJIBBmNodXJjaA!5m1!1e1#",
+      },
+    ],
+  },
+  {
+    city: "Рахів",
+    churches: [
+      {
+        name: "Храм Вознесення Господнього ",
+        address: "вул.Б.Хмельницького ( парк ім.Т.Шевченка) ",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7568.888541640172!2d24.204738153856844!3d48.05720440460533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4737a91edbd65f15%3A0x4d9c34b44d4151f6!2sKaplytsya%20Voznesinnya%20Hospodn%CA%B9oho%20Relehiynoi%20Hromady%20Ukrains%CA%B9koi%20Ptskp!5e0!3m2!1sru!2sua!4v1650280812606!5m2!1sru!2sua",
+        img: "https://www.google.com/maps/place/Kaplytsya+Voznesinnya+Hospodn%CA%B9oho+Relehiynoi+Hromady+Ukrains%CA%B9koi+Ptskp/@48.0581298,24.2087469,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipNVQWsV6f_O9W7KRLDAN1ECp5JXWNsjndEIIv86!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNVQWsV6f_O9W7KRLDAN1ECp5JXWNsjndEIIv86%3Dw114-h86-k-no!7i4160!8i3120!4m9!1m2!2m1!1z0YDQsNGF0ZbQsiDRhtC10YDQutCy0LA!3m5!1s0x4737a91edbd65f15:0x4d9c34b44d4151f6!8m2!3d48.0581298!4d24.2087469!15sChfRgNCw0YXRltCyINGG0LXRgNC60LLQsFoZIhfRgNCw0YXRltCyINGG0LXRgNC60LLQsJIBBmNoYXBlbJoBI0NoWkRTVWhOTUc5blMwVkpRMEZuU1VOak9EaHRhbEIzRUFF!5m1!1e1#",
+      },
+    ],
+  },
+  {
+    city: "с.Косівська Поляна",
+    churches: [
+      {
+        name: "Храм Собору 12-ти Апостолів ",
+        address: "вул.Ставище,1224 А",
+        src: "Немає на карті",
+        img: "Немає зображення",
+      },
+    ],
+  },
+  {
+    city: "с.Ділове",
+    churches: [
+      {
+        name: "Храм Успіння Пресвятої Богородиці ",
+        address: "вул.Визволення,4 А",
+        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d922.3949765004501!2d24.17284536937064!3d47.93650233743482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4737ad6376f7aa1f%3A0xfa68558e6b3e16a4!2sTserkva%20Rizdva%20Presvyatoyi%20Bohorodytsi!5e0!3m2!1sru!2sua!4v1650281238070!5m2!1sru!2sua",
+        img: "https://www.google.com/maps/place/Tserkva+Rizdva+Presvyatoyi+Bohorodytsi/@47.9372028,24.1733362,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipNA7XABHGu1QgpW4IHx-2wyleGFdNNvWkgfdEV_!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNA7XABHGu1QgpW4IHx-2wyleGFdNNvWkgfdEV_%3Dw114-h86-k-no!7i3200!8i2400!4m13!1m7!3m6!1s0x4737ad9ba1cc6cff:0x9bea79acaba5a42e!2z0JTQtdC70L7QstC-0LUsINCX0LDQutCw0YDQv9Cw0YLRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwgOTA2MjU!3b1!8m2!3d47.9359496!4d24.1736814!3m4!1s0x4737ad6376f7aa1f:0xfa68558e6b3e16a4!8m2!3d47.9371819!4d24.173245!5m1!1e1#",
+      },
+    ],
+  },
+  {
+    city: "смт.Ясіня",
+    churches: [
+      {
+        name: "Храм св.Георгія Переможця ",
+        address: "присілок Кевелів №41",
+        src: "",
+        img: "",
+      },
+      {
+        name: "Свято-Іллінський храм",
+        address: "вул.Возз'єднання, 181 А (Лопушанка)",
+        src: "",
+        img: "",
+      },
+      {
+        name: "Свято-Троїцький храм",
+        address: "вул. Борканюка, 2Б",
+        src: "",
+        img: "",
+      },
+    ],
+  },
+];
+
+<iframe
+  src=""
+  width="600"
+  height="450"
+  style="border:0;"
+  allowfullscreen=""
+  loading="lazy"
+  referrerpolicy="no-referrer-when-downgrade"
+></iframe>;
+
+export default Data;
